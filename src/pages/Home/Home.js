9003f073484d4b2d6ac16aefeec5f6b053dd3bdf
Léo Ségalini <130433746+Lejardinierdu33@users.noreply.github.com>
2023-09-19 12:25:04 +0400

    MAJ

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,7 +8,7 @@ import ScrollImg from "../../components/Scrollimage/ScrollImg";
 import Caroussel from "../../components/Caroussel/Caroussel";
 import Footer from "../../components/Footer/Footer";
 import Contact from "../../components/Contact/Contact";
-import { FaEnvelope, FaPhone } from "react-icons/fa";
+import { FaEnvelope, FaPhone, FaGithub } from "react-icons/fa";
 import { Fade } from "react-awesome-reveal";
 import ScrollToTopButton from "../../components/ScrollToTopButton/ScrollToTopButton"; // Importez le composant ScrollToTopButton
 
@@ -34,6 +34,11 @@ function Home() {
               <div className="text_coo">
                 <a href="tel:+3370963371">
                   <FaPhone style={{ color: iconColor }} /> +33 70 96 33 71
+                </a>
+              </div>
+              <div className="text_coo">
+                <a href="https://github.com/Lejardinierdu33">
+                  <FaGithub style={{ color: iconColor }} /> https://github.com/Lejardinierdu33
                 </a>
               </div>
             </div>
