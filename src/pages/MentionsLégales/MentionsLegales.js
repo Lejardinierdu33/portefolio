65d90af591b043d0c6ca97c6dac54fa5ae021215
Léo Ségalini <130433746+Lejardinierdu33@users.noreply.github.com>
2023-11-07 14:03:56 +0400

    Maj Image

--- src/pages/MentionsLégales/MentionsLegales.js
+++ src/pages/MentionsLégales/MentionsLegales.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Footer from "../../components/Footer/Footer";
 import { NavLink } from "react-router-dom";
-import logo from "../../img/png_logo_black.png";
+import logo from "../../img/png_logo_black.webp";
 
 function MentionsLegales() {
   return (
